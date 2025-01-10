446a119e2d1a670d82535cfeaea681d83166f79e
C-C-Coalback <argarfield18@gmail.com>
2025-01-10 20:17:18 +0100

    Added placeholder for the chat page.

--- conquest_site/chat/urls.py
+++ conquest_site/chat/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.decks, name="decks"),
+    path("", views.chat, name="chat"),
 ]
---
+++ conquest_site/chat/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+
+def chat(request):
+    template = loader.get_template('chat.html')
+    return HttpResponse(template.render())
