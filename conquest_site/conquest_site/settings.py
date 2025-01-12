a24a251f1da8ffe4584a64bd51355e07f7babe9f
C-C-Coalback <argarfield18@gmail.com>
2025-01-12 16:06:52 +0100

    Installed Daphne and fixed things that got broken. Preparing to add Daphne/channels.

--- conquest_site/conquest_site/settings.py
+++ conquest_site/conquest_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+ASGI_APPLICATION = "conquest_site.asgi.application"
