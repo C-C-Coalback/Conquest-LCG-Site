c33f490c35edbd0794ff6b9274df42e180e39c06
C-C-Coalback <argarfield18@gmail.com>
2025-01-13 17:13:21 +0100

    Reverting last commit.

--- conquest_site/play/urls.py
+++ conquest_site/play/urls.py
@@ -3,8 +3,6 @@ from django.views.generic.base import TemplateView
 
 from . import views
 
-
 urlpatterns = [
     path("", TemplateView.as_view(template_name="play.html"), name="play"),
-    path("<str:room_name>/", views.room, name="room"),
 ]
