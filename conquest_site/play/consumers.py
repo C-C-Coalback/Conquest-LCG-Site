de974c6ecce6b01b836f45eb17c2fcbb8fe4a211
C-C-Coalback <argarfield18@gmail.com>
2025-01-15 11:57:30 +0100

    Added small code without breaking anything

--- conquest_site/play/consumers.py
+++ conquest_site/play/consumers.py
@@ -5,8 +5,17 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class LobbyConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]
+        self.room_name = "lobby"
+        self.room_group_name = "lobby"
         print("got to lobby consumer")
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
 
 class GameConsumer(AsyncWebsocketConsumer):
