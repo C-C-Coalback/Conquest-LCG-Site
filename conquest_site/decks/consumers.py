125d4403d1a3d059873c21b912c352d7e9e34745
C-C-Coalback <argarfield18@gmail.com>
2025-01-16 16:06:12 +0100

    Receiving text from browser.

--- conquest_site/decks/consumers.py
+++ conquest_site/decks/consumers.py
@@ -6,8 +6,18 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class DecksConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = "lobby"
-        self.room_group_name = "lobby"
+        self.room_name = "decks"
+        self.room_group_name = "decks"
         self.user = self.scope["user"]
         self.name = self.user.username
         print("got to decks consumer")
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+        print(self.room_name)
+        print(self.name)
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        print("receive:", message)
