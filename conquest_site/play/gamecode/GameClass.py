4d09bedb55270c29b008e0e3c6e513eb0568ce24
C-C-Coalback <argarfield18@gmail.com>
2025-02-24 11:27:50 +0100

    Info box updates to show phase, mode and whose turn it is.

--- conquest_site/play/gamecode/GameClass.py
+++ conquest_site/play/gamecode/GameClass.py
@@ -72,8 +72,19 @@ class Game:
         await self.p2.send_resources()
         await self.p1.send_discard()
         await self.p2.send_discard()
+        await self.send_info_box()
         self.condition_main_game.notify_all()
         self.condition_main_game.release()
+
+    async def send_info_box(self):
+        info_string = "GAME_INFO/INFO_BOX/"
+        info_string += "Phase: " + self.phase + "/"
+        info_string += "Mode: " + self.mode + "/"
+        if self.phase == "DEPLOY":
+            info_string += "Active: " + self.player_with_deploy_turn + "/"
+        elif self.phase == "COMBAT":
+            info_string += "Active: " + self.player_with_combat_turn + "/"
+        await self.game_sockets[0].receive_game_update(info_string)
 
     async def send_planet_array(self):
         planet_string = "GAME_INFO/PLANETS/"
@@ -120,6 +131,7 @@ class Game:
                     if self.p1.has_passed and self.p2.has_passed:
                         print("Both passed, move to warlord movement.")
                         self.phase = "COMMAND"
+                    await self.send_info_box()
             if len(game_update_string) == 3:
                 if game_update_string[0] == "HAND":
                     if name == self.player_with_deploy_turn:
@@ -135,6 +147,7 @@ class Game:
                                     if not self.p2.has_passed:
                                         self.player_with_deploy_turn = self.name_2
                                         self.number_with_deploy_turn = "2"
+                                        await self.send_info_box()
                                 else:
                                     self.p1.aiming_reticle_color = "blue"
                                     self.p1.aiming_reticle_coords_hand = self.card_pos_to_deploy
@@ -148,6 +161,7 @@ class Game:
                                     if not self.p1.has_passed:
                                         self.player_with_deploy_turn = self.name_1
                                         self.number_with_deploy_turn = "1"
+                                        await self.send_info_box()
                                 else:
                                     self.p2.aiming_reticle_color = "blue"
                                     self.p2.aiming_reticle_coords_hand = self.card_pos_to_deploy
@@ -168,6 +182,7 @@ class Game:
                                 if not self.p2.has_passed:
                                     self.player_with_deploy_turn = self.name_2
                                     self.number_with_deploy_turn = "2"
+                                    await self.send_info_box()
                             self.card_pos_to_deploy = -1
                             self.p1.aiming_reticle_color = None
                             self.p1.aiming_reticle_coords_hand = None
@@ -183,6 +198,7 @@ class Game:
                                 if not self.p1.has_passed:
                                     self.player_with_deploy_turn = self.name_1
                                     self.number_with_deploy_turn = "1"
+                                    await self.send_info_box()
                             self.card_pos_to_deploy = -1
                             self.p2.aiming_reticle_color = None
                             self.p2.aiming_reticle_coords_hand = None
@@ -224,6 +240,7 @@ class Game:
                         await self.send_planet_array()
                         self.p1.has_passed = False
                         self.p2.has_passed = False
+                        await self.send_info_box()
         elif self.phase == "COMBAT":
             if len(game_update_string) == 1:
                 if game_update_string[0] == "pass-P1" or game_update_string[0] == "pass-P2":
@@ -256,6 +273,8 @@ class Game:
                                 self.reset_combat_turn()
                                 self.mode = "Normal"
                                 await self.check_combat_end()
+                        else:
+                            await self.send_info_box()
             if len(game_update_string) == 4:
                 print("Unit clicked on.")
                 if game_update_string[0] == "IN_PLAY":
@@ -330,6 +349,7 @@ class Game:
                                     await self.p2.send_units_at_planet(self.defender_planet)
                                     self.reset_combat_positions()
                                     self.p1.has_passed = False
+                                    await self.send_info_box()
                                 elif self.number_with_combat_turn == "2":
                                     attack_value = self.p2.get_attack_given_pos(self.attacker_planet,
                                                                                 self.attacker_position)
@@ -349,6 +369,7 @@ class Game:
                                     await self.p1.send_units_at_planet(self.defender_planet)
                                     self.reset_combat_positions()
                                     self.p2.has_passed = False
+                                    await self.send_info_box()
         self.condition_main_game.notify_all()
         self.condition_main_game.release()
 
@@ -362,7 +383,7 @@ class Game:
         p1_has_units = self.p1.check_if_units_present(self.last_planet_checked_for_battle)
         p2_has_units = self.p2.check_if_units_present(self.last_planet_checked_for_battle)
         if p1_has_units and p2_has_units:
-            pass
+            await self.send_info_box()
         else:
             if p1_has_units:
                 print("Player 1 wins battle")
@@ -373,7 +394,6 @@ class Game:
                     self.p1.capture_planet(self.last_planet_checked_for_battle,
                                            self.planet_cards_array)
                     self.planets_in_play_array[self.last_planet_checked_for_battle] = False
-
                     await self.p1.send_victory_display()
             if p2_has_units:
                 print("Player 2 wins battle")
@@ -399,6 +419,7 @@ class Game:
                 self.planet_aiming_reticle_active = True
                 self.planet_aiming_reticle_position = self.last_planet_checked_for_battle
                 await self.send_planet_array()
+                await self.send_info_box()
             else:
                 self.phase = "HEADQUARTERS"
                 self.automated_headquarters_phase()
@@ -412,6 +433,7 @@ class Game:
                 await self.p2.send_resources()
                 await self.p2.send_hand()
                 await self.send_planet_array()
+                await self.send_info_box()
 
     def reset_values_for_new_round(self):
         self.p1.has_passed = False
