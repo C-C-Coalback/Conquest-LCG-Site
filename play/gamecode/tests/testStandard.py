758944037618a6d9f2071cc7b047257c1af70c79
C-C-Coalback <argarfield18@gmail.com>
2025-05-10 21:11:25 +0200

    Simple deployment test.

--- play/gamecode/tests/testStandard.py
+++ play/gamecode/tests/testStandard.py
@@ -21,12 +21,28 @@ with open(second_deck_location, 'r') as file:
 
 
 class StandardTest(unittest.IsolatedAsyncioTestCase):
-    async def test_setup(self):
+    async def test_basic(self):
         self.assertEqual(True, True)
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array)
         await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
         await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        self.assertEqual(len(test_game.p1.cards), 7)
+        self.assertEqual(len(test_game.p2.cards), 7)
+        self.assertEqual(test_game.p1.resources, 7)
+        self.assertEqual(test_game.p2.resources, 7)
+        test_game.p1.draw_card()
+        self.assertEqual(len(test_game.p1.cards), 8)
+        test_game.p2.draw_card()
+        self.assertEqual(len(test_game.p2.cards), 8)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = ["10th Company Scout"]
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["PLANETS", "3"])
+        self.assertEqual(test_game.p1.resources, 6)
+        self.assertEqual(len(test_game.p1.cards), 0)
 
 
 if __name__ == "__main__":
