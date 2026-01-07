189f42c1c7bca8549144e8658fe0cc9c9ec6bdb0
C-C-Coalback <argarfield18@gmail.com>
2026-01-07 13:13:52 +0100

    Fix armorbane

--- play/gamecode/Inits/BlackstoneProjectCardsInit.py
+++ play/gamecode/Inits/BlackstoneProjectCardsInit.py
@@ -5,6 +5,6 @@ def blackstone_project_cards_init():
     blackstone_project_cards = [
         CardClasses.ArmyCard("Sicarian Ruststalker", "Armorbane.",
                              "Soldier. Skitarii.", 2, "Astra Militarum", "Common",
-                             2, 2, 1, False),
+                             2, 2, 1, False, armorbane=True),
     ]
     return blackstone_project_cards
