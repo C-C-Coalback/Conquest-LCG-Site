141e6d125fe8d515b55df3393d6e4d7465e2fc3c
C-C-Coalback <argarfield18@gmail.com>
2026-01-09 21:43:08 +0100

    Emergency hotfix

--- play/gamecode/Interrupts/StartInterrupt.py
+++ play/gamecode/Interrupts/StartInterrupt.py
@@ -347,21 +347,21 @@ async def start_resolving_interrupt(self, name, game_update_string):
             self.delete_interrupt()
         elif current_interrupt == "Growing Tide":
             num, planet_pos, unit_pos = self.positions_of_units_interrupting[0]
-            if planet_pos != 6 and unit_pos != -1:
-                primary_player.cards_in_play[planet_pos + 1][unit_pos].misc_ability_used = False
-                primary_player.remove_damage_from_pos(planet_pos, unit_pos, 999, healing=True)
-                primary_player.increase_attack_of_unit_at_pos(planet_pos, unit_pos, 1, expiration="EOG")
-                primary_player.increase_health_of_unit_at_pos(planet_pos, unit_pos, 1, expiration="EOG")
-                primary_player.move_unit_to_planet(planet_pos, unit_pos, planet_pos + 1)
-                await self.send_update_message("The Growing Tide grows...")
-            elif planet_pos != 6:
-                card = FindCard.find_card("Growing Tide", self.card_array, self.cards_dict,
-                                          self.apoka_errata_cards, self.cards_that_have_errata)
-                if primary_player.add_card_to_planet(card, planet_pos + 1) != -1:
-                    last_element_index = len(primary_player.cards_in_play[planet_pos + 1]) - 1
-                    primary_player.increase_attack_of_unit_at_pos(planet_pos + 1, last_element_index, 1, expiration="EOG")
-                    primary_player.increase_health_of_unit_at_pos(planet_pos + 1, last_element_index, 1, expiration="EOG")
+            if self.round_number != 6:
+                if planet_pos != 6 and unit_pos != -1:
+                    primary_player.remove_damage_from_pos(planet_pos, unit_pos, 999, healing=True)
+                    primary_player.increase_attack_of_unit_at_pos(planet_pos, unit_pos, 1, expiration="EOG")
+                    primary_player.increase_health_of_unit_at_pos(planet_pos, unit_pos, 1, expiration="EOG")
+                    primary_player.move_unit_to_planet(planet_pos, unit_pos, self.round_number + 1)
                     await self.send_update_message("The Growing Tide grows...")
+                elif planet_pos != 6:
+                    card = FindCard.find_card("Growing Tide", self.card_array, self.cards_dict,
+                                              self.apoka_errata_cards, self.cards_that_have_errata)
+                    if primary_player.add_card_to_planet(card, self.round_number + 1) != -1:
+                        last_element_index = len(primary_player.cards_in_play[self.round_number + 1]) - 1
+                        primary_player.increase_attack_of_unit_at_pos(self.round_number + 1, last_element_index, 1, expiration="EOG")
+                        primary_player.increase_health_of_unit_at_pos(self.round_number + 1, last_element_index, 1, expiration="EOG")
+                        await self.send_update_message("The Growing Tide grows...")
             else:
                 await self.send_update_message("No planet to move to!")
             self.delete_interrupt()
