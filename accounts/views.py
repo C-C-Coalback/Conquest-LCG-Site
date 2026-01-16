0be23b583d8256b331c471482328a75c1383f706
C-C-Coalback <argarfield18@gmail.com>
2026-01-16 11:24:18 +0100

    Add chaos cardbacks

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ from django.shortcuts import redirect
 import os
 
 
-valid_cardbacks = ["Cardback", "Space_Marines_Cardback", "Necrons_Cardback"]
+valid_cardbacks = ["Cardback", "Space_Marines_Cardback", "Necrons_Cardback", "Chaos_Cardback"]
 valid_backgrounds = ["Imperial Aquila"]
 if not os.path.exists(os.path.join(os.getcwd(), "user_preferences_storage/")):
     os.mkdir(os.path.join(os.getcwd(), "user_preferences_storage/"))
