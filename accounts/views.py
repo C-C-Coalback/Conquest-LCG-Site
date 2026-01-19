fa9a63336d5628b0316feec48b9e575c4d1c3c9b
C-C-Coalback <argarfield18@gmail.com>
2026-01-19 10:02:04 +0100

    Eldar, Dark Eldar and Tau cardbacks added

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,8 @@ import os
 
 
 valid_cardbacks = ["Cardback", "Space_Marines_Cardback", "Necrons_Cardback", "Chaos_Cardback", "Tyranids_Cardback",
-                   "Orks_Cardback", "Astra_Militarum_Cardback"]
+                   "Orks_Cardback", "Astra_Militarum_Cardback", "Dark_Eldar_Cardback",
+                   "Eldar_Cardback", "Tau_Cardback"]
 valid_backgrounds = ["Imperial Aquila"]
 if not os.path.exists(os.path.join(os.getcwd(), "user_preferences_storage/")):
     os.mkdir(os.path.join(os.getcwd(), "user_preferences_storage/"))
