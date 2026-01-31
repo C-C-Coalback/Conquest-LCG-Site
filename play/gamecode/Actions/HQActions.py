a8e63713b50b79bbb57a2205d7407f4128293c09
C-C-Coalback <argarfield18@gmail.com>
2026-01-31 21:39:23 +0100

    Munitorum Support leaves action mode.

--- play/gamecode/Actions/HQActions.py
+++ play/gamecode/Actions/HQActions.py
@@ -103,6 +103,7 @@ async def update_game_event_action_hq(self, name, game_update_string):
                                 self.name_player_making_choices = primary_player.name_player
                                 self.position_of_actioned_card = (planet_pos, unit_pos)
                                 self.resolving_search_box = True
+                                self.mode = "Normal"
                     elif ability == "Mycetic Spores":
                         if card.get_ready():
                             self.action_chosen = ability
