71ea649328067882e933bdfa330f13ac4b844786
C-C-Coalback <argarfield18@gmail.com>
2026-02-01 17:03:25 +0100

    Add chat room back. Chat room messages are saved when site is updated.

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,14 +1,11 @@
 import json
+import os
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-chat_messages = [[], []]
-
-
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        global chat_messages
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
         self.user = self.scope["user"]
@@ -22,9 +19,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
         print(self.room_name)
-        for i in range(len(chat_messages[0])):
-            if chat_messages[0][i] == self.room_name:
-                await self.send(text_data=json.dumps({"message": chat_messages[1][i]}))
+        room_logs_path = os.path.join(os.getcwd(), "chat/chat_logs/" + self.room_name)
+        if os.path.exists(room_logs_path):
+            with open(room_logs_path, "r") as logs_file:
+                logs_text = logs_file.read()
+            logs_split = logs_text.split(sep="\n")
+            if len(logs_split) > 50:
+                logs_split = logs_split[-50:]
+            for i in range(len(logs_split)):
+                if logs_split[i]:
+                    await self.send(text_data=json.dumps({"message": logs_split[i]}))
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -32,15 +36,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        global chat_messages
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         message = self.name + ": " + message
+        message = message.replace("\n", "")
         print("receive:", message)
-        chat_messages[0].append(self.room_name)
-        chat_messages[1].append(message)
-        print(chat_messages)
-
+        room_logs_path = os.path.join(os.getcwd(), "chat/chat_logs/" + self.room_name)
+        if not os.path.exists(os.path.join(os.getcwd(), "chat/chat_logs")):
+            os.mkdir(os.path.join(os.getcwd(), "chat/chat_logs"))
+        if os.path.exists(room_logs_path):
+            with open(room_logs_path, "a") as logs_file:
+                logs_file.write(message + "\n")
+        else:
+            with open(room_logs_path, "w") as logs_file:
+                logs_file.write(message + "\n")
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat.message", "message": message}
