9c9259c0db60cc612552c34bb582b8b9598e02bf
C-C-Coalback <argarfield18@gmail.com>
2026-02-17 16:30:20 +0100

    Add apoka AOE unit test.

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -110,6 +110,28 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["PLANETS", "0"])
         self.assertEqual(len(test_game.stored_damage), 2)
 
+    async def test_area_effect_apoka(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "Apoka", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Tactical Squad Cardinis"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Tactical Squad Cardinis"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Tactical Squad Cardinis"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Tactical Squad Cardinis"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Tactical Squad Cardinis"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Tactical Squad Cardinis"), 0)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "2"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "3"])
+        self.assertEqual(len(test_game.stored_damage), 3)
+
     async def test_flying(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
