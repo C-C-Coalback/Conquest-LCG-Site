3db5d0464e3d3ee257faa2c843322473e4548dc3
C-C-Coalback <argarfield18@gmail.com>
2026-04-20 10:50:42 +0200

    Unit test for The Fury of Sicarius

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -169,6 +169,26 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P2", ["pass-P1"])
         self.assertEqual(test_game.p1.get_ready_given_pos(0, 1), False)
 
+    async def test_fury_sicarius(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Vicious Bloodletter"), 0)
+        test_game.p1.cards = ["The Fury of Sicarius"]
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["CHOICE", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(len(test_game.p2.cards_in_play[1]), 1)
+        self.assertEqual(test_game.p1.get_resources(), 5)
+
 
 if __name__ == "__main__":
     unittest.main()
