a1208b43617006761afb18bba3134a8fe6f6249b
C-C-Coalback <argarfield18@gmail.com>
2026-04-20 11:30:16 +0200

    Add more attachment unit tests

--- play/gamecode/tests/testGenericAttachments.py
+++ play/gamecode/tests/testGenericAttachments.py
@@ -25,7 +25,7 @@ with open(second_deck_location, 'r') as file:
     deck_content_2 = file.read()
 
 
-class AttachmentsTest(unittest.IsolatedAsyncioTestCase):
+class GenericAttachmentsTest(unittest.IsolatedAsyncioTestCase):
     async def test_deploy_attachment_to_own_in_play(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
@@ -120,6 +120,19 @@ class AttachmentsTest(unittest.IsolatedAsyncioTestCase):
         test_game.p1.attach_card(test_game.preloaded_find_card("Hot-Shot Laspistol"), 0, 0)
         self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 5)
 
+    async def test_attachment_extra_command(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Promotion"), 0, 0)
+        self.assertEqual(test_game.p1.get_command_given_pos(0, 0), 2)
+
     async def test_unique_unit_only_attachment(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
@@ -148,4 +161,82 @@ class AttachmentsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["HAND", "1", "0"])
         await test_game.update_game_event("P1", ["HQ", "1", "0"])
         self.assertEqual(test_game.p1.get_armorbane_given_pos(-2, 0), True)
+
+    async def test_ranged_granting_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Rokkit Launcha"), 0, 0)
+        self.assertEqual(test_game.p1.get_ranged_given_pos(0, 0), True)
+
+    async def test_flying_granting_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Valkyris Pattern Jump Pack"), 0, 0)
+        self.assertEqual(test_game.p1.get_flying_given_pos(0, 0), True)
+
+    async def test_retaliate_granting_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Vitarus, the Sanguine Sword"), 0, 0)
+        self.assertEqual(test_game.p1.get_retaliate_given_pos(0, 0), 3)
+
+    async def test_lumbering_granting_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Centurion Warsuit"), 0, 0)
+        self.assertEqual(test_game.p1.get_lumbering_given_pos(0, 0), True)
+
+    async def test_immune_to_enemy_events_granting_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Shoota Mob"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Lucky Warpaint"), 0, 0)
+        self.assertEqual(test_game.p1.get_immune_to_enemy_events(0, 0), True)
+
+    async def test_additional_traits_granting_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Shoota Mob"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Lucky Warpaint"), 0, 0)
+        self.assertEqual(test_game.p1.check_for_trait_given_pos(0, 0, "Blue"), True)
 
