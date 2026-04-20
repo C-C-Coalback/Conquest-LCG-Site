581b7af505c63b70318b2d27df78e403e837fbe8
C-C-Coalback <argarfield18@gmail.com>
2026-04-20 11:19:36 +0200

    Add more attachment unit tests

--- play/gamecode/tests/testAttachments.py
+++ play/gamecode/tests/testAttachments.py
@@ -89,3 +89,63 @@ class AttachmentsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p2.get_all_attachments_at_pos(-2, 1)), 1)
         self.assertEqual(test_game.p1.resources, 6)
         self.assertEqual(len(test_game.p1.cards), 0)
+
+    async def test_attachment_extra_health(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Godwyn Pattern Bolter"), 0, 0)
+        self.assertEqual(test_game.p1.get_health_given_pos(0, 0), 2)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Hostile Environment Gear"), 0, 0)
+        self.assertEqual(test_game.p1.get_health_given_pos(0, 0), 5)
+
+    async def test_attachment_extra_attack(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Godwyn Pattern Bolter"), 0, 0)
+        self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 3)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Hot-Shot Laspistol"), 0, 0)
+        self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 5)
+
+    async def test_unique_unit_only_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = ["Tallassarian Tempest Blade"]
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["HQ", "1", "0"])
+        self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(0, 0)), 0)
+        self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(-2, 0)), 1)
+
+    async def test_armorbane_granting_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = ["Tallassarian Tempest Blade"]
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["HQ", "1", "0"])
+        self.assertEqual(test_game.p1.get_armorbane_given_pos(-2, 0), True)
+
