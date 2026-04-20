985b95d4b14de77208fea0c0fa911ab1f650054e
C-C-Coalback <argarfield18@gmail.com>
2026-04-20 11:41:53 +0200

    Add more attachment unit tests

--- play/gamecode/tests/testGenericAttachments.py
+++ play/gamecode/tests/testGenericAttachments.py
@@ -324,3 +324,35 @@ class GenericAttachmentsTest(unittest.IsolatedAsyncioTestCase):
         test_game.p1.attach_card(test_game.preloaded_find_card("Regeneration"), 0, 0)
         self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(0, 0)), 1)
 
+    async def test_required_unit_faction_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Void Pirate"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Warriors of Gidrim"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Gauss Flayer"), 0, 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Gauss Flayer"), 0, 1)
+        self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(0, 0)), 0)
+        self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(0, 1)), 1)
+
+    async def test_required_card_type_of_unit_attachment(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Ripper Swarm"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Regeneration"), 0, 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Regeneration"), 0, 1)
+        self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(0, 0)), 1)
+        self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(0, 1)), 0)
+
