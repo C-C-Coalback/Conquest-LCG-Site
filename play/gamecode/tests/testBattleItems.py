9bced4a4606b529e91bbdae266540173b90a7878
C-C-Coalback <argarfield18@gmail.com>
2026-04-23 21:01:31 +0200

    Add unit test for Junk Chucka Kommando

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -283,6 +283,35 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards), 0)
         self.assertEqual(test_game.p1.get_ready_given_pos(0, 1), True)
 
+    async def test_junk_chucka_kommando(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Zarathur's Flamers"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Junk Chucka Kommando"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Rokkit Launcha"), 0, 1)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["ATTACHMENT", "IN_PLAY", "1", "0", "1", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(len(test_game.p2.cards_in_play[1]), 1)
+        self.assertEqual(len(test_game.p1.discard), 1)
+        self.assertEqual(len(test_game.p2.discard), 1)
+        self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(0, 1)), 0)
+
 
 
 if __name__ == "__main__":
