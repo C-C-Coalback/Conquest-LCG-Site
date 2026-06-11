063c7a6fe0502e6784d32940aa6a06d1f87e022a
C-C-Coalback <argarfield18@gmail.com>
2026-06-11 19:22:13 +0200

    Add Straken's Command Squad test

--- play/gamecode/tests/testReactions.py
+++ play/gamecode/tests/testReactions.py
@@ -349,5 +349,23 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p2.get_damage_given_pos(1, 1), 0)
         self.assertEqual(test_game.p1.get_damage_given_pos(1, 0), 1)
 
+    async def test_strakens_command_squad(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            new_warlord_deck_content = file.read()
+        await test_game.p1.setup_player(new_warlord_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Straken's Command Squad"), 0)
+        test_game.p1.destroy_card_in_play(0, 0)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(test_game.p1.get_ability_given_pos(0, 0), "Guardsman")
+
+
 if __name__ == "__main__":
     unittest.main()
