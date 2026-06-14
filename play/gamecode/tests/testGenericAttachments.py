4c936bdb305d1af1c77a0d8ab30f22fd3388ed51
C-C-Coalback <argarfield18@gmail.com>
2026-06-14 19:48:35 +0200

    Add Straken's Cunning test.

--- play/gamecode/tests/testGenericAttachments.py
+++ play/gamecode/tests/testGenericAttachments.py
@@ -371,6 +371,29 @@ class GenericAttachmentsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards), 0)
         self.assertEqual(len(test_game.p1.attachments_at_planet[4]), 1)
 
+    async def test_strakens_cunning(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Ripper Swarm"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Straken's Cunning"), 0, 0)
+        test_game.p1.destroy_card_in_play(0, 0)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(len(test_game.p1.cards), 3)
+        test_game.p1.cards = []
+        test_game.p1.add_to_hq(test_game.preloaded_find_card("Ripper Swarm"))
+        test_game.p1.attach_card(test_game.preloaded_find_card("Straken's Cunning"), -2, 1)
+        test_game.p1.destroy_card_in_play(-2, 1)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(len(test_game.p1.cards), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
