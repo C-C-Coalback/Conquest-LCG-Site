344afab4daa5dba1a9af33a9a3c6d59221f2035d
C-C-Coalback <argarfield18@gmail.com>
2026-06-14 19:52:52 +0200

    Add Captain Markis test.

--- play/gamecode/tests/testInPlayActions.py
+++ play/gamecode/tests/testInPlayActions.py
@@ -59,4 +59,24 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards), 0)
         self.assertEqual(len(test_game.p1.cards_in_play[1]), 3)
 
+    async def test_captain_markis(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        card = test_game.preloaded_find_card("Captain Markis")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Eager Recruit"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Penal Legionnaire"), 0)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "2"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        self.assertEqual(len(test_game.p1.cards_in_play[1]), 2)
+        self.assertEqual(test_game.p2.get_ready_given_pos(0, 1), False)
+        self.assertEqual(test_game.p1.get_once_per_phase_used_given_pos(0, 1), True)
+
 
