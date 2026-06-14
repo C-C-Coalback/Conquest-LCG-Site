1a559825b338c3614299f2dd6d13a2524eb959b3
C-C-Coalback <argarfield18@gmail.com>
2026-06-14 20:09:19 +0200

    Add simple Bodyguard test.

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -408,6 +408,28 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["pass-P1"])
         self.assertEqual(test_game.p1.discard, ["Blood Angels Veterans"])
 
+    async def test_bodyguard_one_bodyguard(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        card = test_game.preloaded_find_card("Blood Angels Veterans")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(card, 0)
+        test_game.p2.attach_card(test_game.preloaded_find_card("Bodyguard"), 0, 1)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 2)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
+
+
 
 if __name__ == "__main__":
     unittest.main()
