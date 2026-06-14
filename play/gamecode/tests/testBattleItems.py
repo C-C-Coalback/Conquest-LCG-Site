06b52a134350d3fc36579a89451dc41fca691e98
C-C-Coalback <argarfield18@gmail.com>
2026-06-14 18:24:00 +0200

    Add Glorious Intervention test

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -250,6 +250,37 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p1.get_damage_given_pos(0, 1), 1)
         self.assertEqual(len(test_game.stored_damage), 0)
 
+    async def test_glorious_intervention(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Zarathur's Flamers"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Infantry Conscripts"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Guardsman"), 0)
+        test_game.p1.cards = ["Glorious Intervention"]
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["IN_PLAY", "2", "0", "1"])
+        await test_game.update_game_event("P2", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        self.assertEqual(test_game.choice_context, "Use alternative shield effect?")
+        await test_game.update_game_event("P1", ["CHOICE", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        self.assertEqual(len(test_game.p1.cards_in_play[1]), 3)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "2"])
+        self.assertEqual(len(test_game.p1.cards_in_play[1]), 2)
+        self.assertEqual(len(test_game.stored_damage), 1)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
+        self.assertEqual(test_game.p1.get_resources(), 6)
+        self.assertEqual(len(test_game.p1.cards), 0)
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 0)
+
     async def test_iron_halo(self):
         random.seed(42)
         with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
