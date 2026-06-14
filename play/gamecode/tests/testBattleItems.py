7a34327a9a5e9c8d1d2dae5dcdc93825b5ca62aa
C-C-Coalback <argarfield18@gmail.com>
2026-06-14 20:11:52 +0200

    Add Rockcrete Bunker test.

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -429,6 +429,26 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 2)
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
 
+    async def test_rockcrete(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        card = test_game.preloaded_find_card("Blood Angels Veterans")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p2.add_to_hq(test_game.preloaded_find_card("Rockcrete Bunker"))
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        await test_game.update_game_event("P2", ["HQ", "2", "0"])
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 2)
+        self.assertEqual(test_game.p2.get_damage_given_pos(-2, 0), 1)
 
 
 if __name__ == "__main__":
