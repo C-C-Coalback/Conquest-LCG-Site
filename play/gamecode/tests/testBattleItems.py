3cdb18b8d28d68123ab27e33f9e8972deb10fe34
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 11:20:02 +0200

    Add Battle Cry test.

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -499,6 +499,26 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 5)
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
 
+    async def test_battle_cry(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        card = test_game.preloaded_find_card("Burna Boyz")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Land Raider"), 0)
+        test_game.p1.cards = ["Battle Cry"]
+        test_game.p1.resources = 7
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        self.assertEqual(test_game.p1.resources, 4)
+        self.assertEqual(test_game.p1.get_attack_given_pos(0, 1), 7)
+        self.assertEqual(test_game.p1.get_attack_given_pos(0, 2), 3)
 
 
 if __name__ == "__main__":
