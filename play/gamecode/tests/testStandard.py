f51fb8849aacb282c6b971c85e377d0c052c2969
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 21:27:01 +0200

    Add Splintered Path Acolyte test.

--- play/gamecode/tests/testStandard.py
+++ play/gamecode/tests/testStandard.py
@@ -240,6 +240,21 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
         self.assertEqual(test_game.p1.resources, 6)
 
+    async def test_splintered_path_acolyte(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = ["Black Legion Heldrake"]
+        test_game.p1.resources = 7
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Splintered Path Acolyte"), 0)
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        self.assertEqual(test_game.p1.resources, 1)
+
 
 if __name__ == "__main__":
     unittest.main()
