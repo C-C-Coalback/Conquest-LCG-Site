5d77769feb47b68c008c2a620402660db4548f8e
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 21:29:04 +0200

    Add Umbral Preacher test.

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -499,6 +499,31 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 5)
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
 
+    async def test_umbral_preacher(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        card = test_game.preloaded_find_card("Umbral Preacher")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Land Raider"), 0)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(len(test_game.p1.cards_in_play[1]), 1)
+        self.assertEqual(len(test_game.p2.cards_in_play[1]), 2)
+
 
 if __name__ == "__main__":
     unittest.main()
