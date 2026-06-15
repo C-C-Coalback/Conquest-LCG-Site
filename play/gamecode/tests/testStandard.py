816a187b3dfa8eecbdf5fd7190df814b2f9a61c7
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 11:09:02 +0200

    Add Crushface test.

--- play/gamecode/tests/testStandard.py
+++ play/gamecode/tests/testStandard.py
@@ -224,6 +224,22 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p1.resources, 7)
         self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 1)
 
+    async def test_crushface(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = ["Enraged Ork"]
+        test_game.p2.cards = []
+        test_game.p1.resources = 7
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Crushface"), 0)
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        self.assertEqual(test_game.p1.resources, 6)
+
 
 if __name__ == "__main__":
     unittest.main()
