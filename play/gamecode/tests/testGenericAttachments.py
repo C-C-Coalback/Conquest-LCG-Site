fba45b59d27016b3c9d1bae39733e18dcbf4ea96
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 10:53:17 +0200

    Add Cybork Body test.

--- play/gamecode/tests/testGenericAttachments.py
+++ play/gamecode/tests/testGenericAttachments.py
@@ -394,6 +394,22 @@ class GenericAttachmentsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["CHOICE", "0"])
         self.assertEqual(len(test_game.p1.cards), 3)
 
+    async def test_cybork_body(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Ripper Swarm"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Cybork Body"), 0, 0)
+        self.assertEqual(test_game.p1.get_health_given_pos(0, 0), 2)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Enraged Ork"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Cybork Body"), 0, 1)
+        self.assertEqual(test_game.p1.get_health_given_pos(0, 1), 10)
+
 
 if __name__ == "__main__":
     unittest.main()
