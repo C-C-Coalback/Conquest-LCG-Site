56ae17f4ee0c9287c42b509170a440978079a7a1
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 11:01:28 +0200

    Add Weirdboy Maniak test.

--- play/gamecode/tests/testReactions.py
+++ play/gamecode/tests/testReactions.py
@@ -423,6 +423,31 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["CHOICE", "0"])
         self.assertEqual(test_game.p1.get_ability_given_pos(0, 0), "Elysian Assault Team")
 
+    async def test_weirdboy_maniak(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            new_warlord_deck_content = file.read()
+        await test_game.p1.setup_player(new_warlord_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Nazdreg's Flash Gitz"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Nazdreg's Flash Gitz"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Nazdreg's Flash Gitz"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Weirdboy Maniak"), 0)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 1)
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 1), 1)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 1)
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 2), 0)
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
