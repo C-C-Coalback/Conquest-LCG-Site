aa2bb89ba8ce59732763410105043892c24a5cde
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 21:15:03 +0200

    Add Zarathur's Flamers test.

--- play/gamecode/tests/testInPlayActions.py
+++ play/gamecode/tests/testInPlayActions.py
@@ -94,4 +94,20 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p2.get_ready_given_pos(0, 1), False)
         self.assertEqual(test_game.p1.get_once_per_phase_used_given_pos(0, 1), True)
 
+    async def test_zarathurs_flamers(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Zarathur's Flamers"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Zarathur's Flamers"), 0)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        self.assertEqual(len(test_game.p1.cards_in_play[1]), 0)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 2)
 
