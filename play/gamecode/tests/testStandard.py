6f9c652296c06c901bd64c876cefc6224a9a116c
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 10:50:04 +0200

    Add Bigga Is Betta test.

--- play/gamecode/tests/testStandard.py
+++ play/gamecode/tests/testStandard.py
@@ -207,6 +207,23 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards_in_play[1]), 1)
         self.assertEqual(len(test_game.p1.cards_in_play[2]), 1)
 
+    async def test_bigga_is_betta(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = ["Bigga Is Betta", "Enraged Ork"]
+        test_game.p2.cards = []
+        test_game.p1.resources = 7
+        await test_game.update_game_event("P1", ["HAND", "1", "1"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        self.assertEqual(len(test_game.p1.cards), 0)
+        self.assertEqual(test_game.p1.resources, 7)
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
