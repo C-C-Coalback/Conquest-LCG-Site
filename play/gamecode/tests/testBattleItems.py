a85fff3c90279b7c3e7ddc8aa8f75d97dd2d52c3
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 11:17:53 +0200

    Add Burna Boyz test.

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -475,6 +475,31 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
         self.assertEqual(6, test_game.p2.get_damage_given_pos(0, 1))
 
+    async def test_burna_boyz(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        card = test_game.preloaded_find_card("Burna Boyz")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Land Raider"), 0)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 5)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
+
+
 
 if __name__ == "__main__":
     unittest.main()
