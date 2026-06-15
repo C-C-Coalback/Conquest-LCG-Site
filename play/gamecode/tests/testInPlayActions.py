d773468b54f2d5c1860e70e586c58a242db05304
C-C-Coalback <argarfield18@gmail.com>
2026-06-15 10:47:26 +0200

    Add Nazdreg's Flash Gitz test.

--- play/gamecode/tests/testInPlayActions.py
+++ play/gamecode/tests/testInPlayActions.py
@@ -59,6 +59,21 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards), 0)
         self.assertEqual(len(test_game.p1.cards_in_play[1]), 3)
 
+    async def test_nazdregs_flash_gitz(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p1.cards = []
+        test_game.p1.resources = 7
+        card = test_game.preloaded_find_card("Nazdreg's Flash Gitz")
+        test_game.p1.add_card_to_planet(card, 0, already_exhausted=True)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 1), 1)
+        self.assertEqual(test_game.p1.get_ready_given_pos(0, 1), True)
+
     async def test_captain_markis(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
