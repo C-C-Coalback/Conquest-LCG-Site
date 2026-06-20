57cd995a07e1cabd7b6d28ea9b9316378b7b9d5b
C-C-Coalback <argarfield18@gmail.com>
2026-06-20 17:14:43 +0200

    Bunch of Bodyguard tests.

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -429,6 +429,79 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 2)
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
 
+    async def test_bodyguard_two_bodyguard_same_unit(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        card = test_game.preloaded_find_card("Blood Angels Veterans")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(card, 0)
+        test_game.p2.attach_card(test_game.preloaded_find_card("Bodyguard"), 0, 1)
+        test_game.p2.attach_card(test_game.preloaded_find_card("Bodyguard"), 0, 1)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 1)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 2)
+
+    async def test_bodyguard_two_bodyguard_different_unit_enough_damage(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        card = test_game.preloaded_find_card("Blood Angels Veterans")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(card, 0)
+        test_game.p2.attach_card(test_game.preloaded_find_card("Bodyguard"), 0, 1)
+        test_game.p2.attach_card(test_game.preloaded_find_card("Bodyguard"), 0, 2)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 1)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 2), 1)
+
+    async def test_bodyguard_two_bodyguard_different_unit_not_enough_damage(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        card = test_game.preloaded_find_card("Blood Angels Veterans")
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Cultist"), 0)
+        test_game.p2.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(card, 0)
+        test_game.p2.attach_card(test_game.preloaded_find_card("Bodyguard"), 0, 1)
+        test_game.p2.attach_card(test_game.preloaded_find_card("Bodyguard"), 0, 2)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        self.assertEqual(len(test_game.stored_damage), 0)
+        await test_game.update_game_event("P2", ["IN_PLAY", "2", "0", "1"])
+        self.assertEqual(len(test_game.stored_damage), 1)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 0)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 1), 1)
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 2), 0)
+
     async def test_rockcrete(self):
         random.seed(42)
         with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
