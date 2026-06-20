ac721e3ca111ff463c7da27a76d5eda26e4b977f
C-C-Coalback <argarfield18@gmail.com>
2026-06-20 17:43:28 +0200

    Add Dire Mutation test.

--- play/gamecode/tests/testGenericAttachments.py
+++ play/gamecode/tests/testGenericAttachments.py
@@ -427,6 +427,19 @@ class GenericAttachmentsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.stored_damage), 2)
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 1)
 
+    async def test_dire_mutation(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Zarathur's Flamers"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Dire Mutation"), 0, 0)
+        test_game.p1.exhaust_given_pos(0, 0)
+        await test_game.update_game_event("P1", [])
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 1)
+
 
 
 if __name__ == "__main__":
