26d88fe61b117724204f12123906f68da733c529
C-C-Coalback <argarfield18@gmail.com>
2026-06-20 17:23:53 +0200

    Add Warpstorm test.

--- play/gamecode/tests/testEventActions.py
+++ play/gamecode/tests/testEventActions.py
@@ -334,6 +334,36 @@ class ActionsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p1.resources, 7)
         self.assertEqual(len(test_game.p1.cards), 0)
 
+    async def test_warpstorm(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p2.cards = []
+        test_game.p1.cards = ["Warpstorm"]
+        test_game.p1.resources = 7
+        card = test_game.preloaded_find_card("Sniveling Grot")
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p2.add_card_to_planet(card, 0)
+        test_game.p1.add_card_to_planet(card, 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Promotion"), 0, 1)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(len(test_game.stored_damage), 0)
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 2)
+        self.assertEqual(test_game.p1.get_ability_given_pos(0, 0), "Zarathur, High Sorcerer")
+        self.assertEqual(test_game.p1.get_ability_given_pos(0, 1), "Sniveling Grot")
+        self.assertEqual(test_game.p1.get_attachment_at_pos(0, 1, 0).get_ability(), "Promotion")
+        self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 3)
+        self.assertEqual(test_game.p1.resources, 4)
+        self.assertEqual(len(test_game.p1.cards), 0)
+
     async def test_infernal_gateway(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
