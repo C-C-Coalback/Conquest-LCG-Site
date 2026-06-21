19f66952bb6e9ca12116c379053f283f48504748
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 15:56:39 +0200

    Add Carnivore Pack test.

--- play/gamecode/tests/testInterrupts.py
+++ play/gamecode/tests/testInterrupts.py
@@ -220,6 +220,24 @@ class InterruptsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards_in_play[1]), 1)
         self.assertEqual(test_game.mode, "Normal")
 
+    async def test_carnivore_pack(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Carnivore Pack"), 0)
+        test_game.p1.destroy_card_in_play(0, 0)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(test_game.p1.resources, 10)
+        test_game.p1.add_to_hq(test_game.preloaded_find_card("Carnivore Pack"))
+        test_game.p1.destroy_card_in_play(-2, 1)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(test_game.p1.resources, 13)
+
 
 if __name__ == "__main__":
     unittest.main()
