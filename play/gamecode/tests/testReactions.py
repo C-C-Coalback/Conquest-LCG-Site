331c3729b19798ea525d0a8538fe47c8c19112dc
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 11:47:11 +0200

    Add Swordwind Farseer test.

--- play/gamecode/tests/testReactions.py
+++ play/gamecode/tests/testReactions.py
@@ -705,6 +705,26 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p2.resources, 11)
         self.assertEqual(test_game.p1.headquarters[0].get_counter(), 3)
 
+    async def test_swordwind_farseer(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Swordwind Farseer"), 0)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["SEARCH", "1"])
+        self.assertEqual(len(test_game.p1.cards), 1)
+        test_game.p1.add_to_hq(test_game.preloaded_find_card("Swordwind Farseer"))
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["SEARCH", "1"])
+        self.assertEqual(len(test_game.p1.cards), 2)
+
     async def test_alaitoc_shrine_planet_to_planet(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
