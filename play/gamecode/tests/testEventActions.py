72d749f597edb8dd0a6b5b6514b2d01a92f347f1
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 10:31:26 +0200

    Add Archon's Terror test.

--- play/gamecode/tests/testEventActions.py
+++ play/gamecode/tests/testEventActions.py
@@ -270,6 +270,23 @@ class ActionsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards), 0)
         self.assertEqual(len(test_game.p1.discard), 1)
 
+    async def test_archons_terror(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p1.cards = ["Archon's Terror"]
+        test_game.p2.cards = []
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Land Raider"), 0)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        self.assertEqual(len(test_game.p2.cards_in_play[1]), 1)
+        self.assertEqual(test_game.p1.resources, 5)
+        self.assertEqual(len(test_game.p1.cards), 0)
+        self.assertEqual(len(test_game.p1.discard), 1)
+
     async def test_repent(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
