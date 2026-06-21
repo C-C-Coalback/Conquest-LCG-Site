730a96b747511a0882ba6e51590259e7a240ab4c
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 16:12:33 +0200

    Add Deception test.

--- play/gamecode/tests/testEventActions.py
+++ play/gamecode/tests/testEventActions.py
@@ -788,5 +788,23 @@ class ActionsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p1.cards, [])
         self.assertEqual(test_game.p1.resources, 6)
 
+    async def test_deception(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await standard_setup(test_game)
+        test_game.p1.cards = ["Deception"]
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Fire Warrior Strike Team"), 0)
+        test_game.p2.attach_card(test_game.preloaded_find_card("Ion Rifle"), 0, 0)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        self.assertEqual(test_game.p1.cards, [])
+        self.assertEqual(test_game.p1.resources, 5)
+        self.assertEqual(test_game.p2.cards, ["Fire Warrior Strike Team"])
+        self.assertEqual(test_game.p2.discard, ["Ion Rifle"])
+
+
 if __name__ == "__main__":
     unittest.main()
