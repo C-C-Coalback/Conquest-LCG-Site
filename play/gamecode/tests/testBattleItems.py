05b22519f48958070fecaf5199c057cbf07e14ab
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 12:06:38 +0200

    Add Spiritseer Erathal test.

--- play/gamecode/tests/testBattleItems.py
+++ play/gamecode/tests/testBattleItems.py
@@ -685,6 +685,27 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
         self.assertEqual(test_game.p2.get_damage_given_pos(0, 0), 3)
 
+    async def test_spiritseer_erathal(self):
+        random.seed(42)
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            cato_deck_content = file.read()
+        with open(os.path.join(current_dir, 'decksForTests/NazdregCore.txt')) as file:
+            nazdreg_deck_content = file.read()
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(nazdreg_deck_content, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p2.cards = ["Rogue Trader"]
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Spiritseer Erathal"), 0)
+        test_game.p1.set_damage_given_pos(0, 0, 3)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        self.assertEqual(len(test_game.reactions_needing_resolving), 1)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 2)
+
     async def test_silvered_blade_avengers(self):
         random.seed(42)
         with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
