22bd2af011a5bb4872add61a96260665bbc3e596
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 09:26:01 +0200

    Add Agonizer of Bren test.

--- play/gamecode/tests/testGenericAttachments.py
+++ play/gamecode/tests/testGenericAttachments.py
@@ -440,6 +440,23 @@ class GenericAttachmentsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", [])
         self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 1)
 
+    async def test_agonizer_of_bren(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Zarathur's Flamers"), 0)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Agonizer of Bren"), 0, 0)
+        card = test_game.preloaded_find_card("Khymera")
+        self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 2)
+        test_game.p1.add_to_hq(card)
+        self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 3)
+        test_game.p1.add_card_to_planet(card, 0)
+        self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 4)
+        test_game.p1.add_card_to_planet(card, 1)
+        self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 5)
 
 
 if __name__ == "__main__":
