f76d75e58f0ba8406c5cf1ecc42abe5df49facd9
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 11:28:01 +0200

    Add extra command struggle winnings tests.

--- play/gamecode/tests/testPassiveEffects.py
+++ play/gamecode/tests/testPassiveEffects.py
@@ -166,6 +166,44 @@ class PassiveEffectsTest(unittest.IsolatedAsyncioTestCase):
         test_game.p2.discard.append("Zarathur's Flamers")
         self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 3)
 
+    async def test_extra_card_winnings_from_command(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [], forced_planet_array=["Barlus", "Osus IV", "Elouith", "Ferrin", "Y'varn", "Iridial", "Carnath"])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Void Pirate"), 0)
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(test_game.p1.resources, 7)
+        self.assertEqual(len(test_game.p1.cards), 3)
+
+    async def test_extra_resource_winnings_from_command(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [], forced_planet_array=["Barlus", "Osus IV", "Elouith", "Ferrin", "Y'varn", "Iridial", "Carnath"])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p1.resources = 7
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Rogue Trader"), 0)
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(test_game.p1.resources, 8)
+        self.assertEqual(len(test_game.p1.cards), 2)
 
 
 if __name__ == "__main__":
