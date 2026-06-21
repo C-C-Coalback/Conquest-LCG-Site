4f1f0d20f3904462daaa980219e6590a34de3471
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 10:20:57 +0200

    Add Coliseum Fighters test.

--- play/gamecode/tests/testReactions.py
+++ play/gamecode/tests/testReactions.py
@@ -600,6 +600,23 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["CHOICE", "0"])
         self.assertEqual(len(test_game.p2.cards), 1)
 
+    async def test_coliseum_fighters(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        with open(os.path.join(current_dir, 'decksForTests/CatoCore.txt')) as file:
+            new_warlord_deck_content = file.read()
+        await test_game.p1.setup_player(new_warlord_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p1.discard = ["Raid", "Power from Pain", "Kith's Khymeramasters",]
+        test_game.p2.cards = []
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Coliseum Fighters"), 0)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(test_game.p1.cards, ["Power from Pain"])
+
 
 if __name__ == "__main__":
     unittest.main()
