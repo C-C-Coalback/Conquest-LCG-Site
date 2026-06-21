c64403f6ef5997e1bfc5e9be50dbf7ce3d83c571
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 11:36:54 +0200

    Add Wildrider Squadron test.

--- play/gamecode/tests/testInPlayActions.py
+++ play/gamecode/tests/testInPlayActions.py
@@ -134,6 +134,22 @@ class BattleItemsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p1.resources, 5)
         self.assertEqual(test_game.p1.get_attack_given_pos(0, 1), 6)
 
+    async def test_wildrider_squadron(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        card = test_game.preloaded_find_card("Wildrider Squadron")
+        test_game.p1.add_card_to_planet(card, 1)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "1", "0"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        self.assertEqual(len(test_game.p1.cards_in_play[1]), 2)
+        self.assertEqual(test_game.p1.get_once_per_phase_used_given_pos(0, 1), True)
+
     async def test_ravenous_flesh_hounds(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
