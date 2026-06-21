558a7284f472c57f12660608fb7ec9110c69ebe2
C-C-Coalback <argarfield18@gmail.com>
2026-06-21 12:23:08 +0200

    Add Superiority test.

--- play/gamecode/Reactions/InPlayReaction.py
+++ play/gamecode/Reactions/InPlayReaction.py
@@ -2354,48 +2354,44 @@ async def resolve_in_play_reaction(self, name, game_update_string, primary_playe
                     await self.send_mistarget_message(primary_player.name_player, "Invalid Target",
                                                       current_reaction + " can only target Ally units.")
         elif current_reaction == "Superiority":
-            if game_update_string[1] == "1":
-                player_being_hit = self.p1
-            else:
-                player_being_hit = self.p2
             can_continue = True
             possible_interrupts = []
-            if player_owning_card.name_player == primary_player.name_player:
-                possible_interrupts = secondary_player.intercept_check()
-            if player_owning_card.name_player == secondary_player.name_player:
-                possible_interrupts = secondary_player.interrupt_cancel_target_check(
-                    planet_pos, unit_pos, intercept_possible=True, event=True)
-                if secondary_player.get_immune_to_enemy_card_abilities(planet_pos, unit_pos):
+            if player_owning_card.get_card_type_given_pos(planet_pos, unit_pos) == "Army":
+                if player_owning_card.name_player == primary_player.name_player:
+                    possible_interrupts = secondary_player.intercept_check()
+                if player_owning_card.name_player == secondary_player.name_player:
+                    possible_interrupts = secondary_player.interrupt_cancel_target_check(
+                        planet_pos, unit_pos, intercept_possible=True, event=True)
+                    if secondary_player.get_immune_to_enemy_card_abilities(planet_pos, unit_pos):
+                        can_continue = False
+                        await self.send_update_message("Immune to enemy card abilities.")
+                    elif secondary_player.get_immune_to_enemy_events(planet_pos, unit_pos):
+                        can_continue = False
+                        await self.send_update_message("Immune to enemy events.")
+                if possible_interrupts and can_continue:
                     can_continue = False
-                    await self.send_update_message("Immune to enemy card abilities.")
-                elif secondary_player.get_immune_to_enemy_events(planet_pos, unit_pos):
-                    can_continue = False
-                    await self.send_update_message("Immune to enemy events.")
-            if possible_interrupts and can_continue:
-                can_continue = False
-                await self.send_update_message("Some sort of interrupt may be used.")
-                self.choices_available = possible_interrupts
-                self.choices_available.insert(0, "No Interrupt")
-                self.name_player_making_choices = secondary_player.name_player
-                self.choice_context = "Interrupt Effect?"
-                self.nullified_card_name = current_reaction
-                self.cost_card_nullified = 0
-                self.nullify_string = "/".join(game_update_string)
-                self.first_player_nullified = primary_player.name_player
-                self.nullify_context = "Reaction Event"
-            if can_continue:
-                if player_being_hit.cards_in_play[planet_pos + 1][unit_pos].get_card_type() == "Army":
-                    player_being_hit.cards_in_play[planet_pos + 1][unit_pos].hit_by_superiority = True
-                    card_name = player_being_hit.cards_in_play[planet_pos + 1][unit_pos].get_name()
-                    text = card_name + ", position " + str(planet_pos) \
-                           + " " + str(unit_pos) + " hit by superiority."
-                    await self.send_update_message(text)
-                    primary_player.discard_card_from_hand(primary_player.aiming_reticle_coords_hand)
-                    primary_player.aiming_reticle_coords_hand = None
-                    self.delete_reaction()
-                else:
-                    await self.send_mistarget_message(primary_player.name_player, "Invalid Target",
-                                                      current_reaction + " can only target army units.")
+                    await self.send_update_message("Some sort of interrupt may be used.")
+                    self.choices_available = possible_interrupts
+                    self.choices_available.insert(0, "No Interrupt")
+                    self.name_player_making_choices = secondary_player.name_player
+                    self.choice_context = "Interrupt Effect?"
+                    self.nullified_card_name = current_reaction
+                    self.cost_card_nullified = 0
+                    self.nullify_string = "/".join(game_update_string)
+                    self.first_player_nullified = primary_player.name_player
+                    self.nullify_context = "Reaction Event"
+                if can_continue:
+                        player_owning_card.cards_in_play[planet_pos + 1][unit_pos].hit_by_superiority = True
+                        card_name = player_owning_card.cards_in_play[planet_pos + 1][unit_pos].get_name()
+                        text = card_name + ", position " + str(planet_pos) \
+                               + " " + str(unit_pos) + " hit by superiority."
+                        await self.send_update_message(text)
+                        primary_player.discard_card_from_hand(primary_player.aiming_reticle_coords_hand)
+                        primary_player.aiming_reticle_coords_hand = None
+                        self.delete_reaction()
+            else:
+                await self.send_mistarget_message(primary_player.name_player, "Invalid Target",
+                                                  current_reaction + " can only target army units.")
         elif current_reaction == "Venomthrope Polluter":
             if game_update_string[1] == primary_player.number:
                 if primary_player.check_for_warlord(planet_pos):
--- play/gamecode/tests/testInterrupts.py
+++ play/gamecode/tests/testInterrupts.py
@@ -50,6 +50,34 @@ class InterruptsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.headquarters), 2)
         self.assertEqual(test_game.p1.get_ability_given_pos(-2, 1), "Catachan Outpost")
 
+    async def test_superiority(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        test_game.p1.cards = ["Superiority"]
+        test_game.p2.cards = []
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Rogue Trader"), 0)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        self.assertEqual(test_game.p2.cards_in_play[1][1].hit_by_superiority, True)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(test_game.after_command_struggle, True)
+        self.assertEqual(len(test_game.p1.cards), 0)
+        self.assertEqual(test_game.p1.resources, 6)
+        self.assertEqual(len(test_game.p2.cards), 0)
+        self.assertEqual(test_game.p2.resources, 7)
+
     async def test_shrouded_harlequin(self):
         # FIXME: MAKE SHROUDED HARLEQUIN AN ACTUAL INTERRUPT INSTEAD OF A REACTION
         random.seed(42)
