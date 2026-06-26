d7be06e5e3a6cc487b23767dd90b5d0552cf593c
C-C-Coalback <argarfield18@gmail.com>
2026-06-26 09:34:41 +0200

    Add Nullify test to AI.

--- play/gamecode/tests/testAutomatedElements.py
+++ play/gamecode/tests/testAutomatedElements.py
@@ -643,3 +643,16 @@ class AutomatedElementsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
         await test_game.update_game_event("P1", ["pass-P1"])
         self.assertIn("P2", test_game.last_automated_data_string)
+
+    async def test_nullify(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [], bot_is_present=True)
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(eldorath_deck_content, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = ["Promise of Glory"]
+        test_game.p2.cards = ["Nullify"]
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        self.assertIn("HQ/2/0", test_game.last_automated_data_string)
