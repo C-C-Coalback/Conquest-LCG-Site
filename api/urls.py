9923d03f379f135818c8f2a829f9749d49414848
C-C-Coalback <argarfield18@gmail.com>
2026-07-03 15:42:23 +0200

    Add deck send and request APIs back

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create_bot_room/", views.create_bot_room, name="create_bot_room"),
+    path("send_deck_text/", views.receive_raw_deck_text, name="receive_raw_deck_text"),
+    path("request_deck/", views.request_deck_text_given_name, name="request_deck_text_given_name"),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from play.consumers import create_bot_game
 import os
 from django.views.decorators.csrf import csrf_exempt
+from decks.consumers import deck_check_and_save
 
 
 def index(request):
@@ -46,5 +47,75 @@ def create_bot_room(request):
         response = {
             'status': 'error',
             'error': 'Only POST requests allowed'
+        }
+    return JsonResponse(response)
+
+
+@csrf_exempt
+def receive_raw_deck_text(request):
+    if request.method != "POST":
+        response = {
+            "status": "error",
+            "error": "Not POST request"
+        }
+        return JsonResponse(response)
+    data = request.POST.dict()
+    try:
+        bot_name = data["name"]
+        deck = data["deck_text"]
+        print(deck)
+        result_of_saving = deck_check_and_save(bot_name, deck)
+        response = {
+            "status": result_of_saving["message"]
+        }
+    except KeyError as e:
+        response = {
+            "status": "error",
+            "error": f"Missing required field: {str(e)}"
+        }
+    except Exception as e:
+        response = {
+            "status": "error",
+            "error": "server error 500",
+            "details": str(e)
+        }
+    return JsonResponse(response)
+
+
+@csrf_exempt
+def request_deck_text_given_name(request):
+    if request.method != "POST":
+        response = {
+            "status": "error",
+            "error": "Not POST request"
+        }
+        return JsonResponse(response)
+    data = request.POST.dict()
+    try:
+        bot_name = data["name"]
+        deck_name = data["deck_name"]
+        target_deck_dir = os.getcwd() + "/decks/DeckStorage/" + bot_name + "/" + deck_name
+        if not os.path.exists(target_deck_dir):
+            response = {
+                "status": "error",
+                "error": "Deck does not exist"
+            }
+        else:
+            with open(target_deck_dir, "r") as file:
+                deck_text = file.read()
+            response = {
+                "status": "success",
+                "deck_text": deck_text
+            }
+    except KeyError as e:
+        response = {
+            "status": "error",
+            "error": f"Missing required field: {str(e)}"
+        }
+    except Exception as e:
+        response = {
+            "status": "error",
+            "error": "server error 500",
+            "details": str(e)
         }
     return JsonResponse(response)
