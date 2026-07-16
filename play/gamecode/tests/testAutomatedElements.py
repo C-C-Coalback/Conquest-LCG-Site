5b5c90143bf9b808d2954f2c6077cc9916d58c05
C-C-Coalback <argarfield18@gmail.com>
2026-07-16 08:59:08 +0200

    Add no damage test spiritseer erathal

--- play/gamecode/tests/testAutomatedElements.py
+++ play/gamecode/tests/testAutomatedElements.py
@@ -595,6 +595,24 @@ class AutomatedElementsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["CHOICE", "0"])
         self.assertIn("HAND/2/0", test_game.last_automated_data_string)
 
+    async def test_spiritseer_erathal_no_damage(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [], bot_is_present=True)
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Spiritseer Erathal"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Wailing Wraithfighter"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Wailing Wraithfighter"), 0)
+        test_game.p1.set_damage_given_pos(0, 2, 1)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertIn("IN_PLAY/1/0/2", test_game.last_automated_data_string)
+        self.assertNotIn("IN_PLAY/1/0/3", test_game.last_automated_data_string)
+
+
     async def test_shrouded_harlequin_execution(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [], bot_is_present=True)
