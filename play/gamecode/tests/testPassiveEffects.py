3728fc50a538f544f44996350720cd2c22e29351
C-C-Coalback <argarfield18@gmail.com>
2026-09-09 18:00:07 +0200

    Add Brood Warrior unit test

--- play/gamecode/tests/testPassiveEffects.py
+++ play/gamecode/tests/testPassiveEffects.py
@@ -190,6 +190,18 @@ class PassiveEffectsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p1.resources, 7)
         self.assertEqual(len(test_game.p1.get_all_attachments_at_pos(0, 0)), 1)
 
+    async def test_brood_warriors(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 0)
+        self.assertEqual(test_game.p1.get_command_given_pos(0, 0), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Brood Warriors"), 0)
+        self.assertEqual(test_game.p1.get_command_given_pos(0, 0), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
