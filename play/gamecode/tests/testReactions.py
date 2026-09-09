938b525c8c1d5f9c98b8c80019aa9b799c470f23
C-C-Coalback <argarfield18@gmail.com>
2026-09-09 18:16:09 +0200

    Add Leviathan Hive Ship test

--- play/gamecode/tests/testReactions.py
+++ play/gamecode/tests/testReactions.py
@@ -840,6 +840,26 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["PLANETS", "1"])
         self.assertEqual(test_game.p1.resources, 5)
 
+    async def test_leviathan_hive_ship(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(cato_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        test_game.p1.add_to_hq(test_game.preloaded_find_card("Leviathan Hive Ship"))
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Brood Warriors"), 0)
+        test_game.p1.destroy_card_in_play(0, 0)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["IN_DISCARD", "1", "0"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        self.assertFalse(test_game.p1.get_ready_given_pos(-2, 0))
+        self.assertEqual(test_game.p1.get_ability_given_pos(0, 0), "Brood Warriors")
+        self.assertFalse(test_game.p1.get_ready_given_pos(0, 0))
+
 
 if __name__ == "__main__":
     unittest.main()
