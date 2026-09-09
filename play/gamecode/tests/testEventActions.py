8e8a9f954cfa2eba0571af3d42dea9ea6f98625a
C-C-Coalback <argarfield18@gmail.com>
2026-09-09 17:10:36 +0200

    Add Ferocious Strength unit test

--- play/gamecode/tests/testEventActions.py
+++ play/gamecode/tests/testEventActions.py
@@ -20,16 +20,22 @@ async def standard_setup(test_game):
     test_game.p2.cards = []
 
 
-async def skip_to_battle_first_planet(test_game):
+async def skip_to_battle_first_planet(test_game, tyranid_1=False, tyranid_2=False):
     await standard_setup(test_game)
     await test_game.update_game_event("P1", ["pass-P1"])
     await test_game.update_game_event("P2", ["pass-P1"])
     await test_game.update_game_event("P1", ["PLANETS", "0"])
+    if tyranid_1:
+        await test_game.update_game_event("P1", ["PLANETS", "1"])
     await test_game.update_game_event("P2", ["PLANETS", "0"])
+    if tyranid_2:
+        await test_game.update_game_event("P2", ["PLANETS", "1"])
     await test_game.update_game_event("P1", ["pass-P1"])
     await test_game.update_game_event("P2", ["pass-P1"])
     await test_game.update_game_event("P1", ["pass-P1"])
     await test_game.update_game_event("P2", ["pass-P1"])
+    test_game.p1.cards = []
+    test_game.p2.cards = []
     await test_game.update_game_event("P1", ["pass-P1"])
     await test_game.update_game_event("P2", ["pass-P1"])
 
@@ -806,6 +812,39 @@ class ActionsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p2.cards, ["Fire Warrior Strike Team"])
         self.assertEqual(test_game.p2.discard, ["Ion Rifle"])
 
+    async def test_ferocious_strength_warlord(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p1.cards = ["Ferocious Strength"]
+        test_game.p1.resources = 7
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Ripper Swarm"), 0)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        self.assertFalse(test_game.p1.get_brutal_given_pos(0, 1))
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        self.assertTrue(test_game.p1.get_brutal_given_pos(0, 0))
+        self.assertEqual(test_game.p1.cards, [])
+        self.assertEqual(test_game.p1.resources, 6)
+
+    async def test_ferocious_strength_synapse(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(ooe_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game, tyranid_1=True)
+        test_game.p1.cards = ["Ferocious Strength"]
+        test_game.p1.resources = 7
+        test_game.p1.move_unit_to_planet(1, 0, 0)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        self.assertTrue(test_game.p1.get_brutal_given_pos(0, 1))
+        self.assertEqual(test_game.p1.cards, [])
+        self.assertEqual(test_game.p1.resources, 6)
 
 if __name__ == "__main__":
     unittest.main()
