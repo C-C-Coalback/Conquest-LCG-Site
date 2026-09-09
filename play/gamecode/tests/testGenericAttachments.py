7cb7fe671aeda8317a9f045c9cc36fc84c716b66
C-C-Coalback <argarfield18@gmail.com>
2026-09-09 18:27:08 +0200

    Add Bone Sabres test

--- play/gamecode/tests/testGenericAttachments.py
+++ play/gamecode/tests/testGenericAttachments.py
@@ -519,5 +519,22 @@ class GenericAttachmentsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["CHOICE", "0"])
         self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 4)
 
+    async def test_bone_sabres(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(ooe_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game, tyranid_1=True)
+        test_game.p1.attach_card(test_game.preloaded_find_card("Bone Sabres"), 0, 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("10th Company Scout"), 0)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["CHOICE", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(len(test_game.p1.cards_in_play[1]), 2)
+
 if __name__ == "__main__":
     unittest.main()
