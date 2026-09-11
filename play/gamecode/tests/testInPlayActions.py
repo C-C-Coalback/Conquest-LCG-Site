17b58ad7406d32d4f74e37b5830f1bb19c0cc7c4
C-C-Coalback <argarfield18@gmail.com>
2026-09-11 15:27:15 +0200

    Add Virulent Spore Sacs test

--- play/gamecode/tests/testInPlayActions.py
+++ play/gamecode/tests/testInPlayActions.py
@@ -173,5 +173,18 @@ class InPlayActionsTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P1", ["PLANETS", "0"])
         self.assertEqual(test_game.p1.get_ability_given_pos(0, 1), "Hunter Gargoyles")
 
+    async def test_virulent_spore_sacs(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Virulent Spore Sacs"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Blood Angels Veterans"), 0)
+        await test_game.update_game_event("P1", ["action-button"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "1"])
+        self.assertEqual(len(test_game.stored_damage), 2)
+        self.assertTrue(test_game.infested_planets[0])
+
 if __name__ == "__main__":
     unittest.main()
