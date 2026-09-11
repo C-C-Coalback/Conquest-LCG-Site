4474a2d47f15c204d72c5b399c4e76d5b9025a63
C-C-Coalback <argarfield18@gmail.com>
2026-09-11 15:10:43 +0200

    Add Gravid Tervigon test

--- play/gamecode/tests/testReactions.py
+++ play/gamecode/tests/testReactions.py
@@ -890,6 +890,23 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         await test_game.update_game_event("P2", ["IN_PLAY", "1", "0", "2"])
         self.assertEqual(test_game.p1.get_damage_given_pos(0, 2), 2)
 
+    async def test_gravid_tervigon(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(ooe_deck_content.replace("Savage Warrior Prime", "Gravid Tervigon"), test_game.planet_array)
+        await test_game.p2.setup_player(ooe_deck_content.replace("Savage Warrior Prime", "Gravid Tervigon"), test_game.planet_array)
+        await standard_setup(test_game)
+        test_game.infest_planet(0, test_game.p1)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["PLANETS", "1"])
+        await test_game.update_game_event("P1", ["PLANETS", "2"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        self.assertEqual(len(test_game.p2.cards_in_play[1]), 4)
+        self.assertEqual(len(test_game.p1.cards_in_play[3]), 2)
 
 if __name__ == "__main__":
     unittest.main()
