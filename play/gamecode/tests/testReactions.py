e1f02d444359bf39b42a22a5a5e0a242e5f55e67
C-C-Coalback <argarfield18@gmail.com>
2026-09-11 16:11:19 +0200

    Add Ravenous Haruspex test

--- play/gamecode/tests/testReactions.py
+++ play/gamecode/tests/testReactions.py
@@ -991,5 +991,23 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards_in_play[1]), 3)
         self.assertEqual(test_game.p1.get_ability_given_pos(0, 2), "Termagant")
 
+    async def test_ravenous_haruspex(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Ravenous Haruspex"), 0)
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("10th Company Scout"), 0)
+        await skip_to_battle_first_planet(test_game)
+        test_game.p1.resources = 0
+        test_game.p2.resources = 0
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "0"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertEqual(test_game.p1.resources, 1)
+
 if __name__ == "__main__":
     unittest.main()
