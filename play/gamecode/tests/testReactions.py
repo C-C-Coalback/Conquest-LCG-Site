b6b51ea104419b8b43ee7cc1c2fd8bd90eaec56e
C-C-Coalback <argarfield18@gmail.com>
2026-09-11 15:17:48 +0200

    Add Termagant Sentry test

--- play/gamecode/tests/testReactions.py
+++ play/gamecode/tests/testReactions.py
@@ -925,5 +925,18 @@ class StandardTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.p1.cards_in_play[2]), 1)
         self.assertEqual(len(test_game.p1.cards_in_play[3]), 2)
 
+    async def test_termagant_sentry(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(ooe_deck_content.replace("Savage Warrior Prime", "Venomthrope Polluter"), test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await standard_setup(test_game)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 0, already_exhausted=True)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant Sentry"), 0, already_exhausted=True)
+        test_game.p1.destroy_card_in_play(0, 0)
+        await test_game.update_game_event("P1", [])
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        self.assertTrue(test_game.p1.get_ready_given_pos(0, 0))
+
 if __name__ == "__main__":
     unittest.main()
