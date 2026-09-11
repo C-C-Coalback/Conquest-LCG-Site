c89a5b03d31d767900f7a735fd4ee50064699cf9
C-C-Coalback <argarfield18@gmail.com>
2026-09-11 16:17:39 +0200

    Add Burrowing Trygon test

--- play/gamecode/tests/testPassiveEffects.py
+++ play/gamecode/tests/testPassiveEffects.py
@@ -261,6 +261,25 @@ class PassiveEffectsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(test_game.p1.get_attack_given_pos(0, 0), 6)
         self.assertEqual(test_game.p1.get_health_given_pos(0, 0), 6)
 
+    async def test_burrowing_trygon(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await standard_setup(test_game)
+        test_game.p1.resources = 10
+        test_game.p1.cards = ["Burrowing Trygon"]
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 0)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 1)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 1)
+        test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Termagant"), 2)
+        await test_game.update_game_event("P1", ["HAND", "1", "0"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        self.assertEqual(test_game.p1.resources, 4)
+
     async def test_savage_warrior_prime_not_same_planet(self):
         random.seed(42)
         test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
