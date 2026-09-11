b479e3b30b8ce595a5eb15bfc0c4886d78c0887c
C-C-Coalback <argarfield18@gmail.com>
2026-09-11 15:54:51 +0200

    Add Volatile Pyrovore test

--- play/gamecode/tests/testInterrupts.py
+++ play/gamecode/tests/testInterrupts.py
@@ -3,6 +3,7 @@ from play.gamecode.GameClass import Game
 from play.gamecode import Initfunctions
 import random
 from play.gamecode.tests.deckLoading import deck_content_1, deck_content_2, ooe_deck_content, cato_deck_content, nazdreg_deck_content, eldorath_deck_content, shadowsun_deck_content
+from play.gamecode.tests.shortcuts import skip_to_battle_first_planet
 
 
 card_array = Initfunctions.init_player_cards()
@@ -275,6 +276,22 @@ class InterruptsTest(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(len(test_game.stored_damage), 0)
         self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 2)
 
+    async def test_volatile_pyrovore(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await skip_to_battle_first_planet(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p2.add_card_to_planet(test_game.preloaded_find_card("Volatile Pyrovore"), 0)
+        test_game.p2.set_damage_given_pos(0, 1, 2)
+        await test_game.update_game_event("P1", ["IN_PLAY", "1", "0", "0"])
+        await test_game.update_game_event("P1", ["CHOICE", "1"])
+        await test_game.update_game_event("P1", ["IN_PLAY", "2", "0", "1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        self.assertEqual(test_game.p1.get_damage_given_pos(0, 0), 3)
 
 if __name__ == "__main__":
     unittest.main()
