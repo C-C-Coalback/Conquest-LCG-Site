d74fa4cba3c2f4575ed1611f6352520e8c28b478
C-C-Coalback <argarfield18@gmail.com>
2026-09-11 15:00:35 +0200

    Added Savage Warrior Prime tests

--- play/gamecode/tests/testPassiveEffects.py
+++ play/gamecode/tests/testPassiveEffects.py
@@ -202,6 +202,92 @@ class PassiveEffectsTest(unittest.IsolatedAsyncioTestCase):
         test_game.p1.add_card_to_planet(test_game.preloaded_find_card("Brood Warriors"), 0)
         self.assertEqual(test_game.p1.get_command_given_pos(0, 0), 1)
 
+    async def test_savage_warrior_prime_not_same_planet(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(ooe_deck_content, test_game.planet_array)
+        await test_game.p2.setup_player(deck_content_2, test_game.planet_array)
+        await test_game.update_game_event("P1", ["CHOICE", "0"])
+        await test_game.update_game_event("P2", ["CHOICE", "0"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        self.assertEqual(len(test_game.p1.headquarters), 2)
+        await test_game.update_game_event("P1", ["PLANETS", "1"])
+        self.assertEqual(len(test_game.p1.headquarters), 0)
+
+    async def test_savage_warrior_prime_init_stealing(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(ooe_deck_content, test_game.planet_array)
+        await standard_setup(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["PLANETS", "1"])
+        await test_game.update_game_event("P2", ["PLANETS", "1"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(test_game.player_with_combat_turn, test_game.name_2)
+
+    async def test_savage_warrior_prime_failed_init_stealing(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(ooe_deck_content, test_game.planet_array)
+        await standard_setup(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P2", ["PLANETS", "1"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(test_game.player_with_combat_turn, test_game.name_1)
+
+    async def test_savage_warrior_prime_battle_starting(self):
+        random.seed(42)
+        test_game = Game("NaN", "P1", "P2", card_array, planet_array, cards_dict, "", [])
+        await test_game.p1.setup_player(deck_content_1, test_game.planet_array)
+        await test_game.p2.setup_player(ooe_deck_content, test_game.planet_array)
+        await standard_setup(test_game)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["PLANETS", "0"])
+        await test_game.update_game_event("P2", ["PLANETS", "0"])
+        await test_game.update_game_event("P2", ["PLANETS", "1"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p1.cards = []
+        test_game.p2.cards = []
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        test_game.p1.retreat_unit(0, 0)
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        await test_game.update_game_event("P2", ["CHOICE", "1"])
+        await test_game.update_game_event("P1", ["pass-P1"])
+        await test_game.update_game_event("P2", ["pass-P1"])
+        self.assertEqual(test_game.last_planet_checked_for_battle, 1)
+        self.assertTrue(test_game.check_if_battle_taking_place())
+
 
 if __name__ == "__main__":
     unittest.main()
